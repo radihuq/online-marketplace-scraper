042fa6cef6a475f4f1b8e1d94c1dbf549c4e2e87
Radiun Huq <huqradi@gmail.com>
2019-11-14 11:28:48 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # online-marketplace-scraper
+Scrape an online marketplace for their listings.
+
+### Installation
+Clone this repo to your machine
+`https://github.com/radihuq/online-marketplace-scraper.git`
+
+### Dependencies
+This scraper uses the simple_html_dom_PHP library. You can find the [original project here](https://simplehtmldom.sourceforge.io/), or check out an [uploaded repo here.](https://github.com/samacs/simple_html_dom) 
+
+### Usage
+1. Edit the `$file` variable with the website you wish to scrape.
+2. Edit the `$pages` variable with the maximum number of pages you wish to scrape
+3. Run the script
+
+### Limitations
+This scraper was built for & tested on one online marketplace; it probably will not work with many other online marketplaces.
+
+### Author
+This scraper was created by Radiun Huq. You can reach him at:
+
+`Email: radi@mrhuq.com`
+
+`Twitter: @radiunhuq`
---
+++ online-marketplace-scraper.php
@@ -0,0 +1,65 @@
+<?php
+
+require 'simple_html_dom.php';
+
+$file = "https://thisopenspace.com/spaces/in/Toronto--ON?price_unit=hour&page=";
+$pages = 1; // Maximum number of pages to scrape. Up to 23 pages available as of Nov 14, 2019
+
+$data = array();
+
+for ($x = 1; $x <= $pages; $x++) {
+    $file .= $x;
+    $html = file_get_html($file);
+    
+    foreach($html->find('.space-container') as $spacecontainer) {
+        $venue = new stdClass();
+
+        //find location
+        foreach($spacecontainer->find('.space') as $space) {
+            $datalat = 'data-lat';
+            $datalng = 'data-lng';
+            $venue->datalat = $space->$datalat;
+            $venue->datalng = $space->$datalng;
+        }
+
+        //find photo
+        foreach($spacecontainer->find('.space-photo') as $spacephoto) {
+            $datastyle = 'data-style';
+            preg_match("/\(([^\)]*)\)/", $spacephoto->$datastyle, $aMatches);
+            $venue->img = $aMatches[1];
+        }
+        
+        foreach($spacecontainer->find('.space-content') as $spacecontent) {
+            //find price
+            foreach($spacecontent->find('.dollar-amount') as $dollaramount) { 
+                    $dollaramountClean = str_replace("$","",$dollaramount->plaintext);
+                    $venue->cost = $dollaramountClean;
+            }
+
+            //find URL
+            foreach($spacecontent->find('a[target=_blank]') as $link) {
+                    $linkClean = "https://thisopenspace.com".$link->href;
+                    $venue->link = $linkClean;
+            }     
+
+            //find sqft
+            foreach($spacecontent->find('.gray-dark') as $sqft) {  
+                $cleansqft = preg_replace("/[^0-9]/", "", $sqft->plaintext);
+                if($cleansqft !== "") {
+                    $venue->sqft = $cleansqft;      
+                }
+            }
+
+            //find venue name
+            foreach($spacecontent->find('h4') as $venueName) {
+                    $venueNameClean = $venueName->plaintext;
+                    $venue->name = $venueNameClean;
+            }
+        }
+        array_push($data, $venue);
+    }
+} 
+
+// echo json_encode($data);
+
+?>
